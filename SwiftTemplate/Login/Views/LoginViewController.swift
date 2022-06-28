394b94b29cfd06ebc78c5ca762b1f2c6fca61f74
Cesar Castillo <gabriel.castillotorres@gmail.com>
2022-06-27 23:06:03 -0400

    feat(LogIn): email and password field validation

--- SwiftTemplate/Login/Views/LoginViewController.swift
+++ SwiftTemplate/Login/Views/LoginViewController.swift
@@ -17,12 +17,16 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordObligatoryFieldLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     
+    private var isValidEmail = false
+    private var isValidPassword = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         hideKeyboard()
+        setupTextFields()
     }
-    
+  
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupKeyboardObservers()
@@ -43,7 +47,7 @@ class LoginViewController: UIViewController {
         self.view.addGestureRecognizer(tapGesture)
     }
 
-    @objc func didTapView(gesture: UITapGestureRecognizer) {
+    @objc private func didTapView(gesture: UITapGestureRecognizer) {
         view.endEditing(true)
     }
     
@@ -52,7 +56,7 @@ class LoginViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardDidHideNotification, object: nil)
     }
     
-    @objc func keyboardWillShow(notification: NSNotification) {
+    @objc private func keyboardWillShow(notification: NSNotification) {
         guard let userInfo = notification.userInfo,
               let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
             return
@@ -62,11 +66,85 @@ class LoginViewController: UIViewController {
         scrollView.contentInset = contentInsets
     }
     
-    @objc func keyboardWillHide(notification: NSNotification) {
+    @objc private func keyboardWillHide(notification: NSNotification) {
         scrollView.contentInset = .zero
     }
     
     private func removeObservers() {
         NotificationCenter.default.removeObserver(self)
+    }
+    
+    @IBAction func didTapLogin(_ sender: Any) {
+        let homeVC = HomeViewController(viewModel: HomeViewModel())
+        homeVC.view.backgroundColor = .systemGray
+        navigationController?.present(homeVC, animated: true)
+    }
+}
+
+// MARK: TextField methods
+extension LoginViewController: UITextFieldDelegate {
+    
+    private func setupTextFields() {
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        emailTextField.layer.cornerRadius = 4
+        passwordTextField.layer.cornerRadius = 4
+        passwordTextField.isSecureTextEntry = true
+        emailTextField.addTarget(self, action: #selector(emailTextFieldDidChange), for: .editingChanged)
+        passwordTextField.addTarget(self, action: #selector(passwordTextFieldDidChange), for: .editingChanged)
+    }
+    
+    @objc private func emailTextFieldDidChange(_ textField: UITextField) {
+        guard let email = emailTextField.text else {
+            return
+        }
+        
+        isValidEmail = isValidEmail(email: email)
+        showEmailObligatoryField(isValidEmail: isValidEmail)
+        isEnabledLoginButton()
+    }
+    
+    @objc private func passwordTextFieldDidChange(_ textField: UITextField) {
+        guard let password = passwordTextField.text else {
+            return
+        }
+    
+        isValidPassword = isValidPassword(password: password)
+        showPasswordObligatoryField(isValidPassword: isValidPassword)
+        isEnabledLoginButton()
+    }
+    
+    private func isValidEmail(email: String) -> Bool {
+        let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        isValidEmail = NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: email)
+        return isValidEmail
+    }
+    
+    private func isValidPassword(password: String) -> Bool {
+        let pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&#])[A-Za-z\\d$@$!%*?&#]{6,25}"
+        isValidPassword = NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: password)
+        return isValidPassword
+    }
+    
+    private func showEmailObligatoryField(isValidEmail: Bool) {
+        emailObligatoryFieldLabel.isHidden = isValidEmail
+        emailTextField.layer.borderWidth = isValidEmail ? 0 : 1
+        emailTextField.layer.borderColor = isValidEmail ? .none : UIColor.systemRed.cgColor
+    }
+    
+    private func showPasswordObligatoryField(isValidPassword: Bool) {
+        passwordObligatoryFieldLabel.isHidden = isValidPassword
+        passwordTextField.layer.borderWidth = isValidPassword ? 0 : 1
+        passwordTextField.layer.borderColor = isValidPassword ? .none : UIColor.systemRed.cgColor
+    }
+    
+    private func isEnabledLoginButton() {
+        loginButton.isEnabled = isValidEmail && isValidPassword
+        loginButton.backgroundColor = isValidEmail && isValidPassword ? .systemRed : .systemGray
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
